0a60419c66e44e46e0ac073576e72b652e408d8a
monn <pphan@paragoniu.edu.kh>
2026-02-01 23:12:00 +0700

    added: root of everything

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,16 +2,24 @@ import './bootstrap';
 import '../css/app.css'
 
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createInertiaApp, Link, Head } from '@inertiajs/vue3'
+import Layouts from './Layouts/Layouts.vue';
 
 createInertiaApp({
+    title: (title) => `My App ${title}`,
     resolve: name=> {
         const pages = import.meta.glob('./Pages/**/*.vue', {eager: true})
-        return pages[`./Pages/${name}.vue`]
+        let page = pages[`./Pages/${name}.vue`]
+        page.default.layout = page.default.layout || Layouts
+        return page
     },
     setup({el, App, props, plugin }) {
-        createApp({ render: () => h(App, props)})
-        .use(plugin)
-        .mount(el)
+        const app = createApp({ render: () => h(App, props)})
+        app.config.devtools = true
+        app.use(plugin)
+        app.component('Head', Head)
+        app.component('Link', Link)
+        app.mount(el)
     },
 })
+
