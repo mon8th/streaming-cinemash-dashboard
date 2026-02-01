54dad7b57920a0b220a6e902b58df1dcb717b7fb
monn <pphan@paragoniu.edu.kh>
2026-02-01 23:11:37 +0700

    added: page layout

---
+++ resources/js/Layouts/Layouts.vue
@@ -0,0 +1,16 @@
+<script setup>
+import sidebar from '@/Components/sidebar.vue';
+import navbar from '@/Components/navbar.vue';
+</script>
+
+<template>
+    <div class="flex min-h-screen bg-slate-900">
+        <sidebar />
+        <div class="flex-1 flex flex-col">
+            <navbar />
+            <main class="flex-1 p-6">
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
