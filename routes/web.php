24cd88b61c587cc597fa164fd3a667139fe9adc1
monn <pphan@paragoniu.edu.kh>
2026-02-02 15:26:48 +0700

    added: route for the other 2 sidebar component

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Streaming/index');
+    return Inertia::render('index');
 });
 
 // Route::inertia('/about', 'About', ['user' => 'mon']);
 
+Route::inertia('/streaming', 'streaming', ['user' => 'admin']);
+Route::inertia('/profile', 'profile', ['user' => 'admin']);
