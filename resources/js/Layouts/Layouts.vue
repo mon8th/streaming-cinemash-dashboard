28b8a4361623b84d74251dfa924280386b895310
monn <pphan@paragoniu.edu.kh>
2026-02-02 15:28:58 +0700

    adjustment

--- resources/js/Layouts/Layouts.vue
+++ resources/js/Layouts/Layouts.vue
@@ -1,16 +1,16 @@
 <script setup>
-import sidebar from '@/Components/sidebar.vue';
-import navbar from '@/Components/navbar.vue';
+import Sidebar from '@/Components/Sidebar.vue';
+import Navbar from '@/Components/Navbar.vue';
 </script>
 
 <template>
-    <div class="flex min-h-screen bg-slate-900">
-        <sidebar />
-        <div class="flex-1 flex flex-col">
-            <navbar />
-            <main class="flex-1 p-6">
-                <slot />
-            </main>
-        </div>
+  <div class="flex h-screen bg-slate-900 overflow-hidden">
+    <Sidebar />
+    <div class="flex-1 flex flex-col min-w-0">
+      <Navbar />
+      <main class="flex-1 p-6 overflow-y-auto">
+        <slot />
+      </main>
     </div>
+  </div>
 </template>
