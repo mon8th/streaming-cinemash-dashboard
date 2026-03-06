ba12074558fa86084c95f098be71843858cde562
monn <pphan@paragoniu.edu.kh>
2026-03-06 16:56:25 +0700

    add layout

--- resources/js/Layouts/Layouts.vue
+++ resources/js/Layouts/Layouts.vue
@@ -1,16 +1,16 @@
 <script setup>
-import Sidebar from '@/Components/Sidebar.vue';
-import Navbar from '@/Components/Navbar.vue';
+import sidebar from '@/Components/sidebar.vue';
+import navbar from '@/Components/navbar.vue';
 </script>
 
 <template>
-  <div class="flex h-screen bg-slate-900 overflow-hidden">
-    <Sidebar />
-    <div class="flex-1 flex flex-col min-w-0">
-      <Navbar />
-      <main class="flex-1 p-6 overflow-y-auto">
-        <slot />
-      </main>
+    <div class="flex min-h-screen bg-slate-900">
+        <sidebar />
+        <div class="flex-1 flex flex-col">
+            <navbar />
+            <main class="flex-1 p-6">
+                <slot />
+            </main>
+        </div>
     </div>
-  </div>
 </template>
